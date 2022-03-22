984067e96ee079a9bd0a5f12d8f426485702d802
Kevin Muldoon <100124261+kevinmuldoon-github@users.noreply.github.com>
2022-03-22 15:36:55 +0000

    Experimentations

--- repositories/booking_repository.py
+++ repositories/booking_repository.py
@@ -1,3 +1,5 @@
+import pdb
+
 from db.run_sql import run_sql
 
 from models.booking import Booking
@@ -39,13 +41,23 @@ def select_all():
     bookings = []
     sql = "SELECT * FROM bookings"
     results = run_sql(sql)
-
     for result in results:
         member = member_repository.select(result['member_id'])
         activity = activity_repository.select(result['activity_id'])
         booking = Booking(member , activity , result['id'])
         bookings.append(booking)
     return bookings
+
+# Function to see if booking exists
+def does_booking_already_exist(member_id, activity_id):
+    booking_exists = False
+    sql = "SELECT COUNT(*) FROM bookings WHERE member_id=%s AND activity_id=%s THEN TRUE"
+    values = [member_id, activity_id]
+    result = run_sql(sql, values)
+
+    if result ==0 :
+        booking_exists = True
+    return booking_exists
 
 # Function to delete a specific booking
 def delete(id):
